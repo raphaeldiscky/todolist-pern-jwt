9cb7bd34500bc159e124f9d9c46dc19ef4e4ad6a
Raphael Discky <zundria.putra@gmail.com>
2020-09-12 16:08:30 +0700

    Build our restful API

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -2,19 +2,73 @@ const router = require("express").Router();
 const pool = require("../db");
 const authorization = require("../middleware/authorization");
 
+// Get all todos and name from a user
 router.get("/", authorization, async (req, res) => {
   try {
-    // req.user has a payload
-    // res.json(req.user); //get an id: user_id
+    // const user = await pool.query("SELECT user_name FROM users WHERE user_id = $1", [
+    //   req.user.id,
+    // ]);
 
-    const user = await pool.query("SELECT user_name FROM users WHERE user_id = $1", [
-      req.user.id,
-    ]);
+    const user = await pool.query(
+      "SELECT u.user_name, t.todo_id, t.description FROM users AS u LEFT JOIN todos AS t ON u.user_id = t.user_id WHERE u.user_id = $1",
+      [req.user.id] // from jwtGenerator payload
+    );
 
-    res.json(user.rows[0]); //get user data 
+    res.json(user.rows); //get user data
   } catch (err) {
     console.error(err.message);
     res.status(500).json("Server Error");
+  }
+});
+
+// Create a todo
+router.post("/todos", authorization, async (req, res) => {
+  try {
+    // get data from client side
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todos (user_id, description) VALUES ($1, $2) RETURNING *",
+      [req.user.id, description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// Update a todo
+router.put("/todos/:id", authorization, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todos SET description = $1 WHERE todo_id = $2 AND user_id = $3 RETURNING *",
+      [description, id, req.user.id]
+    );
+    if (updateTodo.rows.length === 0) {
+      return res.json("This todo is not yours");
+    }
+    res.json("Todo was updated");
+  } catch (err) {
+    console.error();
+  }
+});
+
+// Delete a todo
+router.delete("/todos/:id", authorization, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query(
+      "DELETE FROM todos WHERE todo_id = $1 AND user_id = $2 RETURNING *",
+      [id, req.user.id]
+    );
+    if (deleteTodo.rows.length === 0) {
+      return res.json("This todo is not yours")
+    }
+    res.json("Todo was deleted!");
+  } catch (err) {
+    console.error(err.message);
   }
 });
 
