d3e49dcf2d9d1e3b882c973980240681ee11a59f
Raphael Discky <zundria.putra@gmail.com>
2020-10-02 19:17:10 +0700

    Configure main server file

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static('./client/build'));
+
+if (process.env.NODE_ENV === 'production') {
+  // server static content inside build folder
+  app.use(express.static(path.join(__dirname, 'client/build')));
+}
 
 // ROUTES
 // register and login routes
@@ -13,6 +22,6 @@ app.use('/auth', require('./routes/jwtAuth'));
 // dashboard route
 app.use('/dashboard', require('./routes/dashboard'));
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
