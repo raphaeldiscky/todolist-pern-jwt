23e3d73c7f5aaadada93053f56f249b31d96a296
Raphael Discky <zundria.putra@gmail.com>
2020-10-02 22:00:30 +0700

    Fix bug after deploying

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static('./client/build'));
-
 if (process.env.NODE_ENV === 'production') {
   // server static content inside build folder
   app.use(express.static(path.join(__dirname, 'client/build')));
@@ -21,6 +19,10 @@ app.use('/auth', require('./routes/jwtAuth'));
 
 // dashboard route
 app.use('/dashboard', require('./routes/dashboard'));
+
+app.get('*', (res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
