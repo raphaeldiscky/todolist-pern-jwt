f5bf2873ef4aa2a2ae56c1c341f81b661f5f5685
Raphael Discky <zundria.putra@gmail.com>
2020-10-02 20:22:33 +0700

    comment in database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE jwtauthtodo;
+-- CREATE DATABASE jwtauthtodo;
 
 --users
 CREATE TABLE users
@@ -19,27 +19,3 @@ CREATE TABLE todos
     PRIMARY KEY (todo_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
-
---fake users data
-insert into users
-    (user_name, user_email, user_password)
-values
-    ('discky', 'discky@gmail.com', '12345');
-insert into users
-    (user_name, user_email, user_password)
-values
-    ('jacob', 'jacob@gmail.com', '321321');
-
---fake todos data
-insert into todos
-    (user_id, description)
-values
-    ('4068a3d1-82bb-4d40-8775-69dd93366517', 'Eating hotdog');
-
---show discky relation with todos data
-select *
-from users INNER JOIN todos ON users.user_id = todos.user_id;
-
---show discky relation with todos data and show jacob who doesn't have relation with todos data
-select *
-from users LEFT JOIN todos ON users.user_id = todos.user_id;
