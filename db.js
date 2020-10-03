61da65741eac8abff4f7522f482936b5f8b9dd28
Raphael Discky <zundria.putra@gmail.com>
2020-10-03 20:37:04 +0700

    Configure db connection

--- db.js
+++ db.js
@@ -2,21 +2,30 @@
 const Pool = require('pg').Pool;
 require('dotenv').config();
 
-const devConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE
-};
+// const devConfig = {
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_DATABASE
+// };
 
-const proConfig = {
-  connectionString: process.env.DATABASE_URL, // from heroku addons
-  ssl: true
-};
+// const proConfig = {
+//   connectionString: process.env.DATABASE_URL, // from heroku addons
+//   ssl: true
+// };
 
-const pool = new Pool(
-  process.env.NODE_ENV === 'production' ? proConfig : devConfig
-);
+// const pool = new Pool(
+//   process.env.NODE_ENV === 'production' ? proConfig : devConfig
+// );
+
+const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+const proConfig = process.env.DATABASE_URL; //heroku addons
+
+const pool = new Pool({
+  connectionString:
+    process.env.NODE_ENV === 'production' ? proConfig : devConfig
+});
 
 module.exports = pool;
