e0414212bf948e8a9af72897f1d1313a0a8a22c5
Raphael Discky <zundria.putra@gmail.com>
2020-10-03 20:34:11 +0700

    Heroku test 3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 // middleware
 app.use(express.json());
 app.use(cors());
-
+app.use(express.static(path.join(__dirname, 'client/build')));
 if (process.env.NODE_ENV === 'production') {
   // server static content inside build folder
   app.use(express.static(path.join(__dirname, 'client/build')));
@@ -21,7 +21,7 @@ app.use('/auth', require('./routes/jwtAuth'));
 app.use('/dashboard', require('./routes/dashboard'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 app.listen(PORT, () => {
