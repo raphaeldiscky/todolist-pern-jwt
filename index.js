9785cf8886fdf3d2afc1987a7ca2c59e0e7d1ca5
Raphael Discky <zundria.putra@gmail.com>
2020-10-03 20:01:23 +0700

    Test heroku

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
 if (process.env.NODE_ENV === 'production') {
   // server static content inside build folder
   app.use(express.static('client/build'));
